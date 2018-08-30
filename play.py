fa2d86cc3cfc4ee2527186bc6aeac5b9c29954d2
Logan <logna.shaner@gmail.com>
2018-08-30 15:49:13 -0400

    added search ability, added recursive method, organized print.

---
+++ play.py
@@ -0,0 +1,21 @@
+def factorial(n):
+    if n == 1:
+        return 1
+
+    return n * factorial(n - 1)
+
+
+print("5!={:,}, 3!={:,}, 11!={:,}".format(
+    factorial(5),
+    factorial(3),
+    factorial(11)
+))
+
+'''
+def factorial(n):
+    if n <= 1:
+        return 1
+        
+    return n * factorial(n - 1)
+
+'''
--- program.py
+++ program.py
@@ -1,29 +1,40 @@
 import os
+import collections
+
+SearchResult = collections.namedtuple('SearchResult',
+                                      'file, line, text')
 
 
 def main():
     print_header()
     folder = get_folder_from_user()
     if not folder:
-        print("Sorry we can't search that location")
+        print("Sorry we can't search those locations")
         return
-
     text = get_search_text_from_user()
     if not text:
         print("We can't search for nothing")
         return
+    search_folders(folder, text)
 
-    matches = search_folders(folder, text)
+    matches = search_folders(folder,text)
+    for m in matches:
+        # print(m)
+        print('------ MATCH ------')
+        print('file: ' + m.file)
+        print('line: {}'.format(m.line))
+        print('match: ' + m.text.rstrip())
+        print()
 
 
 def print_header():
-    print('-------------------------')
-    print('     File Search App     ')
-    print('-------------------------')
+    print("-------------------")
+    print("  File Search App  ")
+    print("-------------------")
 
 
 def get_folder_from_user():
-    folder = input('What folder do you want to search? ')
+    folder = input('What folder would you like to search? ')
     if not folder or not folder.strip():
         return None
 
@@ -34,20 +45,24 @@ def get_folder_from_user():
 
 
 def get_search_text_from_user():
-    text = input('What are you searching for[single phrases only]? ')
+    text = input('What are you searching for [single phrases only] ? ')
     return text.lower()
 
 
 def search_folders(folder, text):
+    # print("Would search {} for: '{}'".format(folder, text))
+
     all_matches = []
     items = os.listdir(folder)
 
     for item in items:
         full_item = os.path.join(folder, item)
-        if os.path.isabs(item):
-            continue
-        matches  = search_file(full_item, text)
-        all_matches.extend(matches)
+        if os.path.isdir(full_item):
+            matches = search_folders(full_item, text)
+            all_matches.extend(matches)
+        else:
+            matches = search_file(full_item, text)
+            all_matches.extend(matches)
 
     return all_matches
 
@@ -56,12 +71,15 @@ def search_file(filename, search_text):
     matches = []
     with open(filename, 'r', encoding='utf-8') as fin:
 
+        line_num = 0
         for line in fin:
+            line_num += 1
             if line.lower().find(search_text) >= 0:
-                matches.append(line)
+                m = SearchResult(line=line_num, file=filename, text=line)
+                matches.append(m)
+
+        return matches
 
 
 if __name__ == '__main__':
     main()
-
-# this is a comment.
