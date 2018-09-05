5c58d4571e5a0eb51d8add1c2168687e05cfa24c
reckless01 <logan.shaner@gmail.com>
2018-09-05 13:14:58 -0400

    Performance updated, yield additions

--- program.py
+++ program.py
@@ -15,7 +15,7 @@ def main():
     if not text:
         print("We can't search for nothing")
         return
-    search_folders(folder, text)
+    # search_folders(folder, text)
 
     matches = search_folders(folder,text)
     match_count = 0
@@ -57,23 +57,28 @@ def get_search_text_from_user():
 def search_folders(folder, text):
     # print("Would search {} for: '{}'".format(folder, text))
 
-    all_matches = []
+    # all_matches = []
     items = os.listdir(folder)
 
     for item in items:
         full_item = os.path.join(folder, item)
         if os.path.isdir(full_item):
-            matches = search_folders(full_item, text)
-            all_matches.extend(matches)
+            # matches = search_folders(full_item, text)
+            # all_matches.extend(matches)
+            # for m in matches:
+            #   yield m
+            yield from search_folders(full_item, text)
         else:
-            matches = search_file(full_item, text)
-            all_matches.extend(matches)
+            yield from search_file(full_item, text)
+            # matches = search_file(full_item, text)
+            # all_matches.extend(matches)
+            # for m in matches:
+            #   yield m
 
-    return all_matches
+    # return all_matches
 
 
 def search_file(filename, search_text):
-    matches = []
     with open(filename, 'r', encoding='utf-8') as fin:
 
         line_num = 0
@@ -81,9 +86,9 @@ def search_file(filename, search_text):
             line_num += 1
             if line.lower().find(search_text) >= 0:
                 m = SearchResult(line=line_num, file=filename, text=line)
-                matches.append(m)
+                # matches.append(m)
 
-        return matches
+                yield m
 
 
 if __name__ == '__main__':
